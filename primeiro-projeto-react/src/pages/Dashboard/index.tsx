bf2350ce40c42f9243cbf7705edc1ed16ad68a39
jamil2gomes <jamil.lannister23@gmail.com>
2020-08-30 16:32:23 -0300

    Uso da api do github usando axios

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, {useState, FormEvent} from 'react';
 import {FiChevronRight} from 'react-icons/fi';
 import {Titulo, Form, Repositorios} from './styles';
+import api from '../../services/api';
 
 import icon from '../../assets/icon.svg';
 
+interface Repository{
+    full_name: string;
+    description: string;
+    owner:{
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard:React.FC = () => {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState<Repository[]>([]);
+
+    async function handlerAddRepositorio(event:FormEvent<HTMLFormElement>):Promise<void>{
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+        const repositorio = response.data;
+
+        setRepositorios([...repositorios, repositorio]);
+        setNewRepo('');
+    }
+
     return (
         <>
          <img src={icon} alt="Logo github"/>
+
          <Titulo>Explore repositórios no Github</Titulo>
-         <Form>
-             <input placeholder="Digite aqui o nome do repositório"/>
+
+         <Form onSubmit={handlerAddRepositorio}>
+             <input
+             value = {newRepo}
+             onChange ={e => setNewRepo(e.target.value)}
+             placeholder = "Digite aqui o nome do repositório"
+             />
              <button type="submit">Pesquisar</button>
          </Form>
 
          <Repositorios>
-             <a href="teste">
-                <img src="https://avatars0.githubusercontent.com/u/24705347?s=460&u=1bc7052e7b1501bb6e4c34cbf82807adf89fc1b3&v=4"
-                alt="Jamil Gomes"
-                />
+             {
+                 repositorios.map(
+                     repo => (
+                        <a key={repo.full_name} href="teste">
+                        <img src={repo.owner.avatar_url}
+                        alt={repo.owner.login}
+                        />
 
-                <div>
-                    <strong>jamil2gomes/springBootAlgaWorks</strong>
-                    <p>Descrição aqui</p>
-                </div>
+                        <div>
+                            <strong>{repo.full_name}</strong>
+                            <p>{repo.description ===''?'Sem descrição' : repo.description}</p>
+                        </div>
 
-                <FiChevronRight size={20}/>
-             </a>
+                        <FiChevronRight size={20}/>
+                     </a>
+                     ))}
          </Repositorios>
+
         </>
     );
 };
--- primeiro-projeto-react/src/pages/Dashboard/styles.ts
+++ primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -79,6 +79,7 @@ export const Repositorios = styled.div`
 
         div{
             margin-left: 16px;
+            flex: 1;
 
              strong{
                  font-size: 20px;
