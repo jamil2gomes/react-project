63de1c4319691ca22deb14d0860e58c280cde97c
jamil2gomes <jamil.lannister23@gmail.com>
2020-08-30 11:53:59 -0300

    primeiro commit

---
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Titulo} from './styles';
+
+const Dashboard:React.FC = () => {
+    return <Titulo>Dashboard</Titulo>
+};
+
+export default Dashboard;
---
+++ primeiro-projeto-react/src/pages/Detalhes/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Detalhes:React.FC = () => {
+    return <h1>Detalhes</h1>
+};
+
+export default Detalhes;
