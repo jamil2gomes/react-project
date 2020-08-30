0cd7713829e57ab1642a9067537846355f9cb118
jamil2gomes <jamil.lannister23@gmail.com>
2020-08-30 15:21:28 -0300

    estilizando a pagina dashboard

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import {Titulo} from './styles';
+import {FiChevronRight} from 'react-icons/fi';
+import {Titulo, Form, Repositorios} from './styles';
+
+import icon from '../../assets/icon.svg';
 
 const Dashboard:React.FC = () => {
-    return <Titulo>Dashboard</Titulo>
+    return (
+        <>
+         <img src={icon} alt="Logo github"/>
+         <Titulo>Explore repositórios no Github</Titulo>
+         <Form>
+             <input placeholder="Digite aqui o nome do repositório"/>
+             <button type="submit">Pesquisar</button>
+         </Form>
+
+         <Repositorios>
+             <a href="teste">
+                <img src="https://avatars0.githubusercontent.com/u/24705347?s=460&u=1bc7052e7b1501bb6e4c34cbf82807adf89fc1b3&v=4"
+                alt="Jamil Gomes"
+                />
+
+                <div>
+                    <strong>jamil2gomes/springBootAlgaWorks</strong>
+                    <p>Descrição aqui</p>
+                </div>
+
+                <FiChevronRight size={20}/>
+             </a>
+         </Repositorios>
+        </>
+    );
 };
 
 export default Dashboard;
