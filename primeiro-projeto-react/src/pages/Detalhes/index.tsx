22de0e662652563f15959e0b4e84ca5e0c72047b
jamil <jamil.gomes@armateus.com.br>
2020-08-31 10:23:18 -0300

    Estilizando da pagina de detalhes

--- primeiro-projeto-react/src/pages/Detalhes/index.tsx
+++ primeiro-projeto-react/src/pages/Detalhes/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import {useRouteMatch, Link} from 'react-router-dom';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import {Header, RepositorioInfo, Issues} from './styles';
+
+import icon from '../../assets/icon.svg';
 
 interface RouteParam{
     repositorio: string;
@@ -7,7 +11,52 @@ interface RouteParam{
 const Detalhes:React.FC = () => {
     const {params} =  useRouteMatch<RouteParam>();
 
-    return <h1>Detalhe: {params.repositorio}</h1>
+    return (
+        <>
+            <Header>
+                <img src={icon} alt="Logo github"/>
+                <Link to="/">
+                    <FiChevronLeft size={16}/>
+                    Voltar
+                    </Link>
+            </Header>
+
+            <RepositorioInfo>
+                <header>
+                    <img src="" alt="Foto de perfil do Github"/>
+                    <div>
+                        <strong>jamil2gomes/repo</strong>
+                        <p>descricao</p>
+                    </div>
+                </header>
+                <ul>
+                   <li>
+                       <strong>1080</strong>
+                       <span>Starts</span>
+                   </li> 
+                   <li>
+                       <strong>1080</strong>
+                       <span>Forks</span>
+                   </li> 
+                   <li>
+                       <strong>1080</strong>
+                       <span>Forks</span>
+                   </li> 
+                </ul>
+            </RepositorioInfo>
+
+            <Issues>
+                <Link to="jaja">
+                    <div>
+                        <strong>lalala</strong>
+                        <p>lalala</p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </Link>
+            </Issues>
+        </>
+
+    );
 };
 
 export default Detalhes;
